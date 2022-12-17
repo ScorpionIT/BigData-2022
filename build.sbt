2e788642a4a22dd20c33dad5b0ed99bdd60d39c5
ScorpionIT <87262680+ScorpionIT@users.noreply.github.com>
2022-12-17 18:26:09 +0400

    Add files via upload

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "lab4"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+// https://mvnrepository.com/artifact/org.apache.zookeeper/zookeeper
+libraryDependencies += "org.apache.zookeeper" % "zookeeper" % "3.4.14" % "compile"
---
+++ src/main/scala/Philosophers/MainPhilosopher.scala
@@ -0,0 +1,43 @@
+package philosophers
+
+import java.util.concurrent.Semaphore
+
+import org.apache.log4j.BasicConfigurator
+
+object MainPhilosopher {
+  def main(args: Array[String]): Unit = {
+    BasicConfigurator.configure()
+    val hostPort = "localhost:2181"
+    val philosophersCount = 5
+    val seats = philosophersCount - 1
+
+    val forks = new Array[Semaphore](philosophersCount)
+    for (j <- 0 until philosophersCount) {
+      forks(j) = new Semaphore(1)
+    }
+
+    val threads = new Array[Thread](philosophersCount)
+    for (id <- 0 until philosophersCount) {
+      threads(id) = new Thread(
+        new Runnable {
+          def run(): Unit = {
+            val i = (id + 1) % philosophersCount
+            val philosopher = Philosopher(id, hostPort, "/philosophers".toString, forks(id), forks(i), seats)
+
+            for (j <- 1 to 2) {
+              philosopher.performEating()
+              philosopher.performThinking()
+            }
+
+          }
+        }
+      )
+
+      threads(id).setDaemon(false)
+      threads(id).start()
+    }
+    for (id <- 0 until philosophersCount) {
+      threads(id).join()
+    }
+  }
+}
